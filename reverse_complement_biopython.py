f1157b320ccf72ef6c2d7d22bd43b57fa3943cf9
grace-reed <36831651+grace-reed@users.noreply.github.com>
2021-01-03 19:23:16 -0800

    Create reverse_complement_biopython.py

---
+++ reverse_complement_biopython.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#reverse_complement_biopython.py
+from Bio.Seq import Seq
+from Bio import SeqIO
+
+#reads a fasta file with SeqIO
+def transcribeYeast():
+    records = [rec.reverse_complement(id="rc_"+rec.id, description="reverse complement") \
+            for rec in SeqIO.parse("yeast.fasta","fasta")]
+   # outputs a new fasta file whose contents 
+   # are the reverse compliment of the sequences 
+   # from the original fasta file
+    SeqIO.write(records, "reverse_complement_yeast.fasta","fasta")
+
+if __name__=="__main__":
+    transcribeYeast()
