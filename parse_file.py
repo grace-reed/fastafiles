4acfabc1dedeb4275493ed45ed76dd5b8d6f424d
grace-reed <36831651+grace-reed@users.noreply.github.com>
2021-01-23 13:51:29 -0800

    Update parse_file.py

--- parse_file.py
+++ parse_file.py
@@ -14,16 +14,16 @@ line_count = 0
 seq = ''
 
 with open(genome_path) as genome:
-  for line in genome:
-  #increment line count by one
-    line_count +1
-    #if the line count is less than five
-    if line_count < 5:
-      #check to see if the line is a header line (starts with >)
-      if re.match('^>', line):
-      #print the header
-        print(line)
+    for line in genome:
+        #increment line count by one
+        line_count +1
+        #if the line count is less than five
+        if line_count < 5:
+        #check to see if the line is a header line (starts with >)
+            if re.match('^>', line):
+                #print the header
+                print(line)
       else:
         #this is a sequence line so append to seq
         seq+=line
-... tbc
+tbc ... 
